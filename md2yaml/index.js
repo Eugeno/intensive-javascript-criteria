1d3c58a337df6eab70fb5349c05c54e6042bf857
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-07-24 12:05:15 +0300

    Парсит критерии в блоки

--- md2yaml/index.js
+++ md2yaml/index.js
@@ -1,9 +1,78 @@
-const commonmark = require(`commonmark`);
+const CommonMark = require(`commonmark`);
+const fs = require(`fs`);
 
-const reader = new commonmark.Parser();
-const writer = new commonmark.HtmlRenderer({sourcepos: true});
-const parsed = reader.parse("Hello *world*"); // parsed is a 'Node' tree
-// transform parsed if you like...
-const result = writer.render(parsed); // result is a String
+const Type = {
+  HEADING: `heading`
+};
 
-console.log(result);
+const mdFilePath = process.argv[2];
+if (!mdFilePath) {
+  throw new Error(`*.md file path is not passed`);
+}
+
+const mdFile = fs.readFileSync(mdFilePath, `utf-8`);
+if (!mdFile) {
+  throw new Error(`Unable to find *.md file: ${mdFilePath}`);
+}
+
+const iterator = function* (node) {
+  for (let it = node.firstChild; (it = it.next) !== null;) {
+    yield it;
+  }
+};
+
+
+const proceed = function*(iterator) {
+  //noinspection EqualityComparisonWithCoercionJS
+  for (let it; !((it = iterator.next()).done); ) {
+    yield it.value;
+  }
+};
+
+const writer = new CommonMark.HtmlRenderer({sourcepos: true});
+const html = () => {
+  const nodes = [];
+  return {
+    build() {
+      const doc = reader.parse(``);
+      for (node of nodes) {
+        doc.appendChild(node);
+      }
+      return writer.render(doc);
+    },
+    append(node) {
+      nodes.push(node);
+    }
+  }
+};
+
+const readCriteria = (node, iterator) => {
+  const name = node.firstChild.literal;
+  const myHtml = html();
+  for (const it of proceed(iterator)) {
+    const next = it.next;
+    if (next && next.type === Type.HEADING) {
+      break;
+    }
+
+    myHtml.append(it);
+  }
+  return {name, html: myHtml};
+};
+
+const walk = (iterator) => {
+  for (const node of proceed(iterator)) {
+    console.log(`${node.type}/${node.level}:${node.literal}`);
+
+    if (node.type === Type.HEADING && node.level === 3) {
+      const criteria = readCriteria(node, iterator);
+      console.dir(criteria);
+    }
+  }
+};
+const reader = new CommonMark.Parser();
+
+const parsed = reader.parse(mdFile);
+
+walk(iterator(parsed));
+writer.render(parsed);
