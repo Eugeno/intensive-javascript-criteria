018bc6d7aceb413f0907658365909c80d8d08a6f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-07-24 17:32:03 +0300

    Исправляет баги

--- md2yaml/index.js
+++ md2yaml/index.js
@@ -5,6 +5,12 @@ const Type = {
   HEADING: `heading`
 };
 
+const Level = {
+  TYPE: 1,
+  SECTION: 2,
+  CRITERIA: 3
+};
+
 const mdFilePath = process.argv[2];
 if (!mdFilePath) {
   throw new Error(`*.md file path is not passed`);
@@ -15,7 +21,7 @@ if (!mdFile) {
   throw new Error(`Unable to find *.md file: ${mdFilePath}`);
 }
 
-const iterator = function* (node) {
+const iterator = function*(node) {
   for (let it = node.firstChild; (it = it.next) !== null;) {
     yield it;
   }
@@ -24,7 +30,7 @@ const iterator = function* (node) {
 
 const proceed = function*(iterator) {
   //noinspection EqualityComparisonWithCoercionJS
-  for (let it; !((it = iterator.next()).done); ) {
+  for (let it; !((it = iterator.next()).done);) {
     yield it.value;
   }
 };
@@ -46,33 +52,75 @@ const html = () => {
   }
 };
 
+const readName = (node) => node.firstChild.literal;
+
 const readCriteria = (node, iterator) => {
-  const name = node.firstChild.literal;
+  const name = readName(node);
   const myHtml = html();
+
   for (const it of proceed(iterator)) {
+    myHtml.append(it);
+
     const next = it.next;
     if (next && next.type === Type.HEADING) {
       break;
     }
-
-    myHtml.append(it);
   }
+
   return {name, html: myHtml};
 };
 
 const walk = (iterator) => {
+  let currentType;
+  let currentSection;
+
+  const map = {};
   for (const node of proceed(iterator)) {
     console.log(`${node.type}/${node.level}:${node.literal}`);
 
-    if (node.type === Type.HEADING && node.level === 3) {
-      const criteria = readCriteria(node, iterator);
-      console.dir(criteria);
+    if (node.type === Type.HEADING) {
+      switch (node.level) {
+        case Level.TYPE:
+          currentType = readName(node);
+          map[currentType] = {};
+          break;
+        case Level.SECTION:
+          currentSection = readName(node);
+          map[currentType][currentSection] = [];
+          break;
+        case Level.CRITERIA:
+          const criterias = map[currentType][currentSection];
+          criterias.push(readCriteria(node, iterator));
+          break;
+        default:
+          throw new Error(`Unknown level: ${node.level}`);
+      }
     }
   }
+
+  return map;
 };
 const reader = new CommonMark.Parser();
 
 const parsed = reader.parse(mdFile);
 
-walk(iterator(parsed));
-writer.render(parsed);
+const iter = iterator(parsed);
+for (const it of proceed(iter)) {
+  const {
+    next: {
+      firstChild: {
+        literal
+      }
+    }
+  } = it;
+  console.log(typeof literal);
+  if (literal && literal.indexOf(`Базовые`) === 0) {
+    break;
+  }
+}
+
+const result = walk(iter);
+
+console.log(result);
+
+
